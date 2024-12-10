bd0d99ebabda22f9eb0b17eda5a443a29e631fa5
UsuarioAula <aula@usal.es>
2024-12-10 18:59:15 +0100

    docs y codigo

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import com.google.gson.Gson;
+/***
+ * 
+ * this is the main class of the program
+ * 
+ * @author Sandia
+ * @version 2.2
+ * 
+ */
+
+public class Main {
+    /**
+     * the main method of the app
+     * 
+     * @param args the command line arguments for the app
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Gson gs =new Gson();
+
+        
+    }
+}
